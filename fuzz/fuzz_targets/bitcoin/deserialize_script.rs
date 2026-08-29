36fc4888b2210ce0d44794c560afcaacd22afae2
satsfy (Renato Britto) <renatobritto@protonmail.com>
2026-08-29 14:56:32 -0300

    break stuff

--- fuzz/fuzz_targets/bitcoin/deserialize_script.rs
+++ fuzz/fuzz_targets/bitcoin/deserialize_script.rs
@@ -7,6 +7,7 @@ use libfuzzer_sys::fuzz_target;
 fn main() {}
 
 fn do_test(data: &[u8]) {
+    if data.first() == Some(&0x41) { panic!("planted crash A"); }
     let script_result: Result<bitcoin::ScriptPubKeyBuf, _> =
         bitcoin::encoding::decode_from_slice(data);
 
