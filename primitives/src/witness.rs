cb6ca1df76b7143e443a432316f6fa648c57764b
satsfy (Renato Britto) <renatobritto@protonmail.com>
2026-09-17 17:54:22 -0300

    primitives: test witness size hint not trusted

--- primitives/src/witness.rs
+++ primitives/src/witness.rs
@@ -1384,6 +1384,30 @@ mod test {
 
     #[test]
     #[cfg(feature = "serde")]
+    fn serde_does_not_trust_size_hint() {
+        use serde::de::value::{Error, SeqAccessDeserializer};
+        use serde::de::{DeserializeSeed, SeqAccess};
+
+        /// Claims to be as long as possible, then yields nothing.
+        struct HugeHint;
+        impl<'de> SeqAccess<'de> for HugeHint {
+            type Error = Error;
+            fn next_element_seed<T: DeserializeSeed<'de>>(
+                &mut self,
+                _: T,
+            ) -> Result<Option<T::Value>, Error> {
+                Ok(None)
+            }
+            fn size_hint(&self) -> Option<usize> { Some(usize::MAX) }
+        }
+
+        let witness: Witness =
+            serde::Deserialize::deserialize(SeqAccessDeserializer::new(HugeHint)).unwrap();
+        assert!(witness.is_empty());
+    }
+
+    #[test]
+    #[cfg(feature = "serde")]
     fn serde_bincode_roundtrips() {
         let original = arbitrary_witness();
         let ser = bincode::serialize(&original).unwrap();
